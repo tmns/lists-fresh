905e8e8d2414ba56c27a76fb28c6573e5a34b85e
tmns <tmn5@pm.me>
2023-08-19 10:09:19 +0800

    Move to AOT builds

--- twind.config.ts
+++ twind.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'https://esm.sh/@twind/core@1.1.3'
+import { defineConfig, Preset } from 'https://esm.sh/@twind/core@1.1.3'
+import { Options } from '$fresh/plugins/twindv1.ts'
 import presetTailwind from 'https://esm.sh/@twind/preset-tailwind@1.1.4'
 import presetAutoprefix from 'https://esm.sh/@twind/preset-autoprefix@1.0.7'
 import presetTypography from '@twind/preset-typography'
 
 export default {
-  ...defineConfig({
+  ...(defineConfig({
     presets: [presetTailwind(), presetAutoprefix(), presetTypography({ defaultColor: 'white' })],
-  }),
+    // deno-lint-ignore no-explicit-any
+  }) as Preset<any>),
   selfURL: import.meta.url,
-}
+} as Options
